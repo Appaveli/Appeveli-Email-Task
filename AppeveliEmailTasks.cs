4d8c8b358d9bbf7952d1370a069b82e5d80497a2
Dominque Terry <terry_dominque@yahoo.com>
2015-03-24 08:48:02 -0400

    Appeveli Email Task Windows Phone
    This is a simple Library to use the Windows Phone Task Manager to send
    emails.

---
+++ AppeveliEmailTasks.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.Phone.Tasks;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppeveliEmailTask
+{
+    class AppeveliEmailTasks
+    {
+        //Create a method to open the EmailComposeTask
+        //Pass five parameters: Subject, Body, Email Recepitant, CC, and Bcc
+        public void sendEmailMessage(string subject, string body, string emailRecepitant, string Cc, string Bcc) 
+        {
+            EmailComposeTask appeveliComposeTask = new EmailComposeTask();
+
+            appeveliComposeTask.Subject = subject;
+            appeveliComposeTask.Body = body;
+            appeveliComposeTask.To = emailRecepitant;
+            appeveliComposeTask.Cc = Cc;
+            appeveliComposeTask.Bcc = Bcc;
+
+            appeveliComposeTask.Show();
+        }
+
+        //Create method for only one recepitant
+        //Only pass three parameters: Subject, Body, Recepitant
+        public void sendToOneReceiptant(string subject, string body, string recepitant)
+        {
+            EmailComposeTask appeveliComposeTask = new EmailComposeTask();
+
+            appeveliComposeTask.Subject = subject;
+            appeveliComposeTask.Body = body;
+            appeveliComposeTask.To = recepitant;
+        }
+
+        
+    }
+}
